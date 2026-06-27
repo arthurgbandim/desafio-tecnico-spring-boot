535a6abd9e06ee0a7a66d3e16618f2c2f14a7a15
Arthur Bandim <arthurgoncalves.bandim@gmail.com>
2026-06-27 09:52:15 -0300

    Início do projeto

---
+++ src/main/java/br/com/treina/recife/sgcm/api/SgpmApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.treina.recife.sgcm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treina/recife/sgcm/api/SgpmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treina.recife.sgcm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
